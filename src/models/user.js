46b147df11dc4c9d5cf53bd1c7004c795e7c40eb
alaomichael <contactleomax@gmail.com>
2021-05-21 02:04:04 +0100

    Updated the model folder by creating user Schema

---
+++ src/models/user.js
@@ -0,0 +1,24 @@
+const mongoose = required('mongoose');
+const {Schema} = mongoose;
+const userSchema = new Schema({
+    firstName:{
+        type:String,
+        required: true
+    },
+    lastName:{
+        type:String,
+        required: true
+    },
+    username:{
+        type:String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type:String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('User',userSchema);
